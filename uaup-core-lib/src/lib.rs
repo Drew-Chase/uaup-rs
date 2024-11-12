ef3d27be600d550fbb1194802da46524923b7fdb
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-12 15:51:51 -0500

    Organize module paths for conditional compilation
    Reorganize the module paths for github, manifest, manifest_data, and update_server to improve code structure and maintainability. This ensures each module is properly scoped and accessible under corresponding features.

--- uaup-core-lib/src/lib.rs
+++ uaup-core-lib/src/lib.rs
@@ -1,6 +1,13 @@
 #[cfg(feature = "github")]
+#[path = "github/github.rs"]
 mod github;
 #[cfg(feature = "manifest")]
+#[path = "manifest/manifest.rs"]
 mod manifest;
+
+#[cfg(feature = "manifest")]
+#[path = "manifest/manifest_data.rs"]
+mod manifest_data;
 #[cfg(feature = "update_server")]
+#[path = "manifest/update_server.rs"]
 mod update_server;
