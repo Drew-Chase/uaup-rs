6d107d2129a6739373c7a1b2d02a4709cec79d24
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-15 11:45:00 -0500

    Integrate CLI argument parsing with logging
    Added `clap` for command-line argument parsing and initialized the logger. CLI arguments are now parsed into the `UaupCliArgs` struct, enhancing the program's functionality and configurability.

--- uaup-cli/src/main.rs
+++ uaup-cli/src/main.rs
@@ -1,3 +1,9 @@
+use clap::Parser;
+
+mod uaup_cli_args;
+
 fn main() {
-    println!("Hello, world!");
+	std::env::set_var("RUST_LOG", "debug");
+	env_logger::init();
+	let v = uaup_cli_args::UaupCliArgs::parse();
 }
